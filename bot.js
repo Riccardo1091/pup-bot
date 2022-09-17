6663514b060a1298f1e3ff3ac726f10426a72708
Riccardo <riccardo.ruggiero1091@gmail.com>
2022-09-17 22:13:39 +0200

    avanzamento bot2

--- bot.js
+++ bot.js
@@ -1,26 +1,41 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  // Attivazione Browser
+  // Attivazione browser
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
   // Navigazione verso pagina prodotto
-  await page.goto('http://testbuy.netsons.org/prodotto/bello-figo-js/', {waitUntil: 'networkidle0'})
+  await page.goto('http://testbuy.netsons.org/prodotto/bello-figo-js/', {waitUntil: 'networkidle2'})
   // Aggiunta carrello
-  await page.click('[name="add-to-cart"]', {waitUntil: 'networkidle2'})
+  await page.click('[name="add-to-cart"]', {waitUntil: 'networkidle0'})
   // Navigazione verso checkout
-  await page.goto('http://testbuy.netsons.org/carrello/', {waitUntil: 'networkidle0'})
+  await page.waitForSelector('.woocommerce-message a')
+  //await page.waitForFunction('document.querySelector(".woocommerce-message a").href.includes("carrello")');
+  await page.goto('http://testbuy.netsons.org/carrello/')
   // Click bottone checkout
   await page.click('a.checkout-button', {waitUntil: 'networkidle0'})
-  // Input Nome
+  // Input nome
   await page.waitForSelector('input#billing_first_name')
   await page.type('input#billing_first_name', 'Riccardo')
-  // Input Cognome
-  await page.waitForSelector('input#billing_last_name')
+  // Input cognome
   await page.type('input#billing_last_name', 'yoyo')
-  // Input Cognome
-  await page.waitForSelector('input#billing_last_name')
+  // Input indirizzo
   await page.type('input#billing_address_1', 'via test, 69')
+  // Input CAP
+  await page.type('input#billing_postcode', '72222')
+  // Input città
+  await page.type('input#billing_city', 'Roma')
+  // Input select provincia
+  await page.waitForSelector('input.select2-search__field')
+  await page.type('input.select2-search__field', 'Roma')
+  // Input telefono
+  await page.waitForSelector('#billing_phone')
+  await page.type('#billing_phone', '3333333333')
+  // Input email
+  await page.type('input#billing_email', 'riccardo.rugi@gmail.com')
+  // Invia ordine
+  await page.click('a#place_order', {waitUntil: 'networkidle0'})
+
 
   //await browser.close();
 })();
